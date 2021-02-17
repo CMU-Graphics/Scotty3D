bbb55ea348de0280877416758f889e604e6b3ce7
TheNumbat <mjslater@andrew.cmu.edu>
2021-02-17 14:28:15 -0500

    buildtools in build_win

--- build_win.bat
+++ build_win.bat
@@ -1,9 +1,22 @@
+
 @echo off
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" (
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" > NUL 2> NUL
+) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" (
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" > NUL 2> NUL
+) else (
+  rem "Could not find visual studio C++ build environment."
+)
+
 mkdir build
 pushd build
 cmake ..
+
 if "%1"=="configure" exit
+
 if [%1]==[] (cmake --build . --config RelWithDebInfo)
+
 if not [%1]==[] (cmake --build . --config %1)
+
 popd
